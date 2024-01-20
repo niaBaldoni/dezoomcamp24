200ae4ec3aeb993c461fb8bcfd9e58f8ae48081e
niaBaldoni <eugenia.v.baldoni@gmail.com>
2024-01-21 00:53:51 +0100

    pipeline now requires Date argument

--- week1/docker_sql/pipeline.py
+++ week1/docker_sql/pipeline.py
@@ -1,5 +1,11 @@
+import sys
+
 import pandas as pandas
+
+print(sys.argv) 
+
+day = sys.argv[1]
 
 # fancy stuff with pandas
 
-print('job finished successfully')
+print(f'job finished successfully for day = {day}')
