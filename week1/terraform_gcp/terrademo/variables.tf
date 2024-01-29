5ff40876efda3ff90bfe6cfe95ec4abae1dce22d
niaBaldoni <eugenia.v.baldoni@gmail.com>
2024-01-29 21:38:22 +0100

    homework push

---
+++ week1/terraform_gcp/terrademo/variables.tf
@@ -0,0 +1,35 @@
+variable "credentials" {
+  description = "My Credentials"
+  default     = "./keys/my-creds.json"
+}
+
+variable "project" {
+  description = "Project"
+  default     = "perfect-science-412620"
+}
+
+variable "location" {
+  description = "Project Location"
+  default     = "EU"
+}
+
+variable "region" {
+  description = "Region"
+  default     = "europe-west6"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  default     = "demo_dataset"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  default     = "perfect-science-412620-demo-bucket"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
+
